65908ee0fe606b2e04272e7ae36bec86470cfe2d
Nikhil Joshua <nikhiljoshua.a@gmail.com>
2021-09-11 02:35:50 +0530

    Add minor functionality

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ function addEntry() {
 	var Entries = document.getElementById('Entries');
 	var task = document.getElementsByName('task');
 	var element = document.createElement('li');
-	if(startTime[0].value == '' || endTime[0].value == ''){
-		alert('Please enter start & finish time');
+	if(startTime[0].value == '' || endTime[0].value == '' || task[0].value == ''){
+		alert('Please enter Start, Finished Time & Task');
 		return;
 	}
 	element.setAttribute('draggable', 'true');
 	element.innerHTML = '<b>[' + formatTime(startTime[0].value) + ' - ' + formatTime(endTime[0].value) + ']</b> <i>' + task[0].value + '</i>';
 	startTime[0].value = endTime[0].value;
-	endTime[0].value = '';
+	task[0].value = '';
 	Entries.append(element);
 }
 
